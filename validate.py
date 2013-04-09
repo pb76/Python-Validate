58e611ab7504c96ee509dc3b90a038ff0acbbea1
Patrik Bernhardsson <patrik@bernhardsson.com>
2013-04-09 18:46:29 +0200

    recursive directory reading
    Reads test dir recursive. Just add argument ./testar when testing
    script.

--- validate.py
+++ validate.py
@@ -1,3 +1,14 @@
 import os
 import sys
 
+if len(sys.argv) != 2:
+	print "Expected folder argument: '", sys.argv[0], " folder path '"
+	sys.exit()
+
+fileList = []
+rootdir = sys.argv[1]
+for root, subFolders, files in os.walk(rootdir):
+	for file in files:
+		fileList.append(os.path.join(root,file))
+
+print fileList
