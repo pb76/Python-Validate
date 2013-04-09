6b6b3daae72c9ef3c71f2acbf129be5a448bca72
Patrik Bernhardsson <patrik@bernhardsson.com>
2013-04-09 21:41:26 +0200

    removes zip
    added part that removes zip after extraction.
    should we have an argument so you could choose if you want to delete?

--- functions.py
+++ functions.py
@@ -5,12 +5,17 @@ def unzipFile( filePath, rootDir, archiveName ):
 	print "Packar upp: ", filePath
 	dest_folder = os.path.join(rootDir, os.path.splitext(archiveName)[0])
 	print "Dest_folder:", dest_folder
+	
 	if not os.path.exists(dest_folder):
 		os.makedirs(dest_folder)
+		
 	zfile = zipfile.ZipFile( filePath, "r" )
+	
 	for info in zfile.infolist():
 		fname = info.filename
 		# decompress each file's data
 		data = zfile.read(fname)
 		#print "FNAME", fname
 		zfile.extract(fname, dest_folder)
+
+	os.remove(filePath)
